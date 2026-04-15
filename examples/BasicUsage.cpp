f1c887937a920ba8cd8d8ef486d99c6c6b00f5ed
Mnsx_x <xx1527030652@gmail.com>
2026-04-15 17:08:01 +0800

    doc: 创建CMakeList

---
+++ examples/BasicUsage.cpp
@@ -0,0 +1,10 @@
+/** 
+ * @file BasicUsage.cpp
+ * @author Mnsx_x <xx1527030652@gmail.com>
+ * @date 2026/4/15
+ * @description 
+ */
+int main() {
+
+    return 0;
+}
---
+++ src/HermesRpc.cpp
@@ -0,0 +1,6 @@
+/** 
+ * @file HermesRpc.cpp
+ * @author Mnsx_x <xx1527030652@gmail.com>
+ * @date 2026/4/15
+ * @description 
+ */
